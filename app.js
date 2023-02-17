3081b09eae9f9d4b48b4eaefc97dae6b1a3ce9a0
martinloman <martin.loman@gmail.com>
2023-02-17 15:52:35 +0100

    More comments and styling.

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const buyButtons = document.querySelectorAll(".buy")
 
 // loopar igenom alla knappar och lägger på en eventlyssnare för klickevent.
 buyButtons.forEach(function (button) {
-  button.addEventListener("click", function (event) {
-    // Man kan komma åt data-attribut på ett element med egenskapen .dataset
-    // Notera att bindestreck ersätts av camelCase. data-product-id har blivit dataset.productId.
+  // Man kan komma åt data-attribut på ett element med egenskapen .dataset
+  // Notera att bindestreck ersätts av camelCase. data-product-id har blivit dataset.productId.
 
+  console.log(
+    "Lägger till eventlyssnare på knapp med data-product-id=",
+    button.dataset.productId
+  )
+  button.addEventListener("click", function (event) {
     // event.target innehåller den knapp som klickats på
     console.log(
       `Den klickade produkten har id = ${event.target.dataset.productId}`
